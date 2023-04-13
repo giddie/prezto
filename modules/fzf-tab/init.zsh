15cbc4bf128b7690fdbe2cf47272e39728a71a89
Paul Dann <pgdann@gmail.com>
2023-04-13 11:47:39 +0100

    Add fzf-tab module

---
+++ modules/fzf-tab/init.zsh
@@ -0,0 +1,29 @@
+#
+# Integrates fzf-tab into Prezto.
+#
+# Authors:
+#   Paul Gideon Dann <pdgiddie@gmail.com>
+#
+# Source module files.
+
+zstyle -d ':completion:*' format
+
+# disable sort when completing `git checkout`
+zstyle ':completion:*:git-checkout:*' sort false
+
+# set descriptions format to enable group support
+zstyle ':completion:*:descriptions' format '[%d]'
+
+# set list-colors to enable filename colorizing
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+
+zstyle ':completion:*:git-checkout:*' fzf-preview
+
+# preview directory's content with exa when completing cd
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
+
+# switch group using `,` and `.`
+zstyle ':fzf-tab:*' switch-group ',' '.'
+
+source "${0:h}/external/fzf-tab/fzf-tab.plugin.zsh" || return 1
+source "${0:h}/external/fzf-tab-source/fzf-tab-source.plugin.zsh" || return 1
